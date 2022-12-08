fc70348794bbefe4d35a4713de398514767fb3df
uv_2001 <65334844+yuvedha-21@users.noreply.github.com>
2022-12-08 12:59:39 +0530

    Delete EmployeeAPI directory

--- EmployeeAPI/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Employee(models.Model):
-    name=models.CharField(max_length=50)
-    designation=models.CharField(max_length=50)
-    city=models.CharField(max_length=50)
--- EmployeeAPI/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from . import models
-from . import serializers
-
-class EmployeeViewset(viewsets.ModelViewSet):
-    queryset=models.Employee.objects.all()
-    serializer_class=serializers.EmployeeSerializer
